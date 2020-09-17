8b19d0f5183c57aa25a555cf1ead8cf7f1620b44
Akash Desai <49414493+asd15@users.noreply.github.com>
2020-09-17 12:17:11 +0530

    Add files via upload

--- recursion_numofoccurence.cpp
+++ recursion_numofoccurence.cpp
@@ -3,6 +3,7 @@
 #include<iostream>
 using namespace std;
 
+//method 1
 void numofoccurence(int a[], int n, int x, int i, int &cnt){ //cnt is taken as reference(&) coz we will print cnt inside main
     if(i == n) return;
 
@@ -12,10 +13,21 @@ void numofoccurence(int a[], int n, int x, int i, int &cnt){ //cnt is taken as r
     numofoccurence(a, n, x, i+1, cnt);
 }
 
+//method 2
+int numofoccurence2(int a[], int n, int x, int i){
+    if(i == n) return 0;
+
+    if(a[i] == x){
+        return 1 + numofoccurence2(a, n, x, i+1);
+    } else{
+        return 0 + numofoccurence2(a, n, x, i+1);
+    }
+}
+
 int main(){
     int cnt=0;
     int a[] = {3,5,3,7,3,9,3};
-    numofoccurence(a, 7, 3, 0, cnt);
-    cout<<cnt;
+    cout<<numofoccurence2(a, 7, 3, 0);
+    //cout<<cnt;
     return 0;
 }
