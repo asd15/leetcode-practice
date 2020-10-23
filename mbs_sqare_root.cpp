0a25a00ac0d0a82c840f41aca21bafe0b0e35392
Akash Desai <49414493+asd15@users.noreply.github.com>
2020-10-23 14:48:24 +0530

    Add files via upload

---
+++ mbs_sqare_root.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+ 
+int mySqrt(int x){
+    if(x==0) return 0;
+
+    int s = 1;
+    int e = x;
+    int ans;
+    while(s<=e){
+        int mid = s + (e-s)/2;
+
+        if(mid == x/mid){
+            return mid;
+        } else if(mid < x/mid){
+            ans = mid;
+            s = mid + 1;
+        } else{
+            e = mid - 1;
+        }
+    }
+    return ans; 
+}
+
+int main(){
+    cout<<mySqrt(10);
+    return 0;
+}
